6a9b089809a3bd9372e0cd94dcd7a08fb72da956
VladimirTambovtsev <tambovcev99@gmail.com>
2024-08-29 09:48:08 +0300

    Initial Commit

---
+++ VTB Deeplinks/VTB_DeeplinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  VTB_DeeplinksApp.swift
+//  VTB Deeplinks
+//
+//  Created by Владимир Тамбовцев on 29.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct VTB_DeeplinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
