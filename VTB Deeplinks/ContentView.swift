8e661ff309cc9e22f52a4f1e8348a286f6201e11
VladimirTambovtsev <tambovcev99@gmail.com>
2024-08-29 10:23:10 +0300

    first commit

--- VTB Deeplinks/ContentView.swift
+++ VTB Deeplinks/ContentView.swift
@@ -2,20 +2,16 @@
 //  ContentView.swift
 //  VTB Deeplinks
 //
-//  Created by Владимир Тамбовцев on 29.08.2024.
+//  Created by Владимир Тамбовцев on 27.08.2024.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            // Replace "https://ift-ibrb1-sharing.vtb.ru/i/..." with your desired URL
+            WebView(url: URL(string: "https://ift-ibrb1-sharing.vtb.ru/i/paymentSbp/AS10000KVF0LMG3V9TVOOALT90HS2JE2")!)
+                .edgesIgnoringSafeArea(.all)
     }
 }
 
